85165cca4d1cf4d175976579be9fedc907b1ab2e
bernardosborges <bernardoserpaborges@proton.me>
2026-01-09 16:04:21 -0300

    refector(auth): add comments

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -9,7 +9,10 @@ from app.services.auth_service import authenticate_user_service, generate_token_
 
 router = APIRouter(prefix=f"{settings.API_PREFIX}/auth", tags=["Auth"])
 
-# ------------ REGISTER USER ENDPOINT ------------
+# -----------------------------------------------
+# ENDPOINT - REGISTER USER
+# -----------------------------------------------
+
 @router.post(
         "/register",
         response_model=UserReadSchema,
@@ -27,7 +30,10 @@ def register_user_endpoint(
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
-# ------------ LOGIN USER ENDPOINT ------------
+# -----------------------------------------------
+# ENDPOINT - LOGIN USER
+# -----------------------------------------------
+
 @router.post(
         "/login",
         summary="Login a user",
