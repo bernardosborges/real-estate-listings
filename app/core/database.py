390d145abebf9ce5bd25ee14bb2d10f3606ed9ac
bernardosborges <bernardoserpaborges@proton.me>
2026-01-15 15:47:49 -0300

    fix(core): add except rollback to get_db

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import SQLAlchemyError
 from app.core.config import settings
+from sqlalchemy import MetaData
+import logging
 
 
 # URL do db (psycopg v3)
@@ -8,14 +11,34 @@ SQLALCHEMY_DATABASE_URL = (
     f"postgresql+psycopg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
 
+naming_convention = {
+    "ix": "ix_%(table_name)s_%(column_0_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata = MetaData(naming_convention=naming_convention)
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True) # Echo shows SQL commands on terminal for debugging
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base(metadata=metadata)
+
+logger = logging.getLogger(__name__)
 
 # Dependency
 def get_db():
-    db = Session()
+    db = SessionLocal()
     try:
         yield db
+    except SQLAlchemyError as e:
+        db.rollback()
+        logger.exception("Database error ocurred, rollback executed.")
+        raise
+    
+    except Exception as e:
+        db.rollback()
+        logger.exception("Unexpected error ocurred, rollback executed.")
+        raise
+
     finally:
         db.close()
