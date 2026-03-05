fde4bf29dbbe9f3bbaee6061fdedfb920cf5dcb9
bernardosborges <bernardoserpaborges@proton.me>
2026-03-05 16:10:11 -0300

    chore: apply pre-commit fixes to the remaining alembic

--- alembic/versions/0d6473579283_add_updated_at_triggers.py
+++ alembic/versions/0d6473579283_add_updated_at_triggers.py
@@ -5,15 +5,14 @@ Revises: 3bc95735cbba
 Create Date: 2026-01-09 11:36:42.441244
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
-revision: str = '0d6473579283'
-down_revision: Union[str, Sequence[str], None] = '3bc95735cbba'
+revision: str = "0d6473579283"
+down_revision: Union[str, Sequence[str], None] = "3bc95735cbba"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
